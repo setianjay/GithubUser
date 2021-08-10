cbf10e44cf626e1fa7f61251776464fc76e403a7
setianjay <hari.setiaji2000@gmail.com>
2021-08-10 23:26:51 +0700

    Create popup menu on HomeActivity

--- app/src/main/java/com/setianjay/githubuser/screens/activity/HomeActivity.kt
+++ app/src/main/java/com/setianjay/githubuser/screens/activity/HomeActivity.kt
@@ -2,7 +2,10 @@ package com.setianjay.githubuser.screens.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.PopupMenu
 import android.widget.Toast
+import com.setianjay.githubuser.R
 import com.setianjay.githubuser.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
@@ -16,7 +19,26 @@ class HomeActivity : AppCompatActivity() {
     
     private fun initListener(){
         binding.ivMenus.setOnClickListener {
-            Toast.makeText(this@HomeActivity, "Under Development", Toast.LENGTH_SHORT).show()
+            showPopupMenu()
         }
+    }
+
+    private fun showPopupMenu(){
+        val popupMenu = PopupMenu(this,binding.ivMenus)
+        popupMenu.menuInflater.inflate(R.menu.popup_menu,popupMenu.menu)
+
+        popupMenu.setOnMenuItemClickListener { item: MenuItem? ->
+            when(item!!.itemId){
+                R.id.favorites -> {
+                    Toast.makeText(this@HomeActivity, "Under Development", Toast.LENGTH_SHORT).show()
+                }
+                R.id.settings -> {
+                    Toast.makeText(this@HomeActivity, "Under Development", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
+
+        popupMenu.show()
     }
 }

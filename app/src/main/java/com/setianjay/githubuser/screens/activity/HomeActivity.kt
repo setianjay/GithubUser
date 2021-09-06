871eac8ede22c954aed4d133a20ccb29453a277a
setianjay <hari.setiaji2000@gmail.com>
2021-09-06 22:59:36 +0700

    Update setTitle logic function in HomeActivity

--- app/src/main/java/com/setianjay/githubuser/screens/activity/HomeActivity.kt
+++ app/src/main/java/com/setianjay/githubuser/screens/activity/HomeActivity.kt
@@ -5,16 +5,31 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.widget.PopupMenu
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.setianjay.githubuser.R
 import com.setianjay.githubuser.databinding.ActivityHomeBinding
+import com.setianjay.githubuser.network.api.ApiService
+import com.setianjay.githubuser.viewmodel.GithubViewModel
+import com.setianjay.githubuser.viewmodel.GithubViewModelFactory
 
 class HomeActivity : AppCompatActivity() {
     private val binding by lazy { ActivityHomeBinding.inflate(layoutInflater) }
+
+    private lateinit var viewModel: GithubViewModel
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        initViewModel()
         initListener()
+        setupObserver()
+    }
+
+    private fun initViewModel(){
+        viewModel = ViewModelProvider(
+            this@HomeActivity,
+            GithubViewModelFactory(ApiService.githubApi)
+        ).get(GithubViewModel::class.java)
     }
     
     private fun initListener(){
@@ -42,7 +57,10 @@ class HomeActivity : AppCompatActivity() {
         popupMenu.show()
     }
 
-    fun setTitle(title: String){
-        binding.tvPage.text = title
+    private fun setupObserver(){
+        // observe for the title and set with value if any update from current fragment
+        viewModel.getTitle().observe(this@HomeActivity){ title ->
+            binding.tvPage.text = title
+        }
     }
 }
--- app/src/main/java/com/setianjay/githubuser/viewmodel/GithubViewModel.kt
+++ app/src/main/java/com/setianjay/githubuser/viewmodel/GithubViewModel.kt
@@ -1,8 +1,16 @@
 package com.setianjay.githubuser.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.setianjay.githubuser.network.repository.GithubRepository
 
 class GithubViewModel(private val repository: GithubRepository): ViewModel() {
+    private val title: MutableLiveData<String> = MutableLiveData()
 
+
+    fun setTitle(title: String){
+        this.title.postValue(title)
+    }
+
+    fun getTitle(): MutableLiveData<String> = title
 }
